d101c9059b0fbca93a97695ad2de7351309d777c
skkif1 <skkif1@gmail.com>
2017-03-23 00:00:31 +0200

    v 2.0

--- wave_algo.c
+++ wave_algo.c
@@ -12,104 +12,104 @@
 
 #include "filler.h"
 
-static void	calc_distance_right(t_player *player, int i, int j)
+static void calc_distance_right(t_player *player, int i, int j)
 {
-	int y;
+    int y;
 
-	y = 0;
-	while (j < player->map_l - 1)
-	{
-		j++;
-		if (player->map[i][j] == '.')
-		{
-			if (y > 9)
-				break ;
-			player->map[i][j] = y++ + '0';
-		}
-		else
-			break ;
-	}
+    y = 0;
+    while (j < player->map_l - 1)
+    {
+        j++;
+        if (player->map[i][j] == '.') {
+            if (y > 9)
+                break ;
+            player->map[i][j] = y++ + '0';
+        } else
+            break;
+    }
 }
 
-static void	calc_distance_left(t_player *player, int i, int j)
+static void calc_distance_left(t_player *player, int i, int j)
 {
-	int y;
+    int y;
 
-	y = 0;
-	while (j > 0)
-	{
-		j--;
-		if (player->map[i][j] == '.')
-		{
-			if (y > 9)
-				break ;
-			player->map[i][j] = y++ + '0';
-		}
-		else
-			break ;
-	}
+    y = 0;
+    while (j > 0)
+    {
+        j--;
+        if (player->map[i][j] == '.') {
+            if (y > 9)
+                break;
+            player->map[i][j] = y++ + '0';
+        }
+            else
+            break;
+    }
 }
 
-static void	calc_distance_top(t_player *player, int i, int j)
+static void calc_distance_top(t_player *player, int i, int j)
 {
-	int y;
+    int y;
 
-	y = 0;
-	while (i < player->map_h - 1)
-	{
-		i++;
-		if (player->map[i][j] == '.')
-		{
-			if (y > 9)
-				break ;
-			player->map[i][j] = y++ + '0';
-		}
-		else
-			break ;
-	}
+    y = 0;
+    while (i < player->map_h - 1)
+    {
+        i++;
+        if (player->map[i][j] == '.') {
+            if (y > 9)
+                break ;
+            player->map[i][j] = y++ + '0';
+
+        } else
+            break;
+    }
 }
 
-static void	calc_distance_bottom(t_player *player, int i, int j)
+static void calc_distance_bottom(t_player *player, int i, int j)
 {
-	int y;
+    int y;
 
-	y = 0;
-	while (i > 0)
-	{
-		i--;
-		if (player->map[i][j] == '.')
-		{
-			if (y > 9)
-				break ;
-			player->map[i][j] = y++ + '0';
-		}
-		else
-			break ;
-	}
+    y = 0;
+    while (i > 0)
+    {
+        i--;
+        if (player->map[i][j] == '.')
+        {
+            if (y > 9)
+                break ;
+            player->map[i][j] = y++ + '0';
+        }else
+            break;
+    }
 }
 
-void		draw_distance(t_player *player)
-{
-	int		i;
-	int		j;
 
-	i = 0;
-	j = 0;
-	while (i < player->map_h)
-	{
-		while (j < player->map_l)
-		{
-			if (player->map[i][j] == player->oponent_p || player->map[i][j]
-				== player->oponent_p + 32)
-			{
-				calc_distance_bottom(player, i, j);
-				calc_distance_left(player, i, j);
-				calc_distance_right(player, i, j);
-				calc_distance_top(player, i, j);
-			}
-			j++;
-		}
-		j = 0;
-		i++;
-	}
+void draw_distance(t_player *player)
+{
+    int i;
+    int j;
+    char oponent;
+
+    if (player->p == 'O')
+        oponent = 'X';
+    else
+        oponent = 'O';
+    i = 0;
+    j = 0;
+    while(i < player->map_h)
+    {
+        while (j < player->map_l)
+        {
+            if (player->map[i][j] == oponent || player->map[i][j] == oponent + 32)
+            {
+                calc_distance_bottom(player, i, j);
+                calc_distance_left(player, i, j);
+                calc_distance_right(player, i, j);
+                calc_distance_top(player, i, j);
+            }
+            j++;
+        }
+        j = 0;
+        i++;
+    }
 }
